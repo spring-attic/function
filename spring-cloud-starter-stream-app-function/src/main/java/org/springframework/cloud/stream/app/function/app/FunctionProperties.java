49435d96c604a06439802b615f40bafe7370086b
Eric Bottard <eric.bottard+github@gmail.com>
2017-08-25 15:50:02 +0200

    Initial version of the app.
    * Add whitelist properties file.
    * Add Bean Validation constraints
    
    Fixes spring-cloud-stream-app-starters/app-starters-release#104
    Fixes spring-cloud-stream-app-starters/app-starters-release#105
    Fixes spring-cloud-stream-app-starters/app-starters-release#106

---
+++ spring-cloud-starter-stream-app-function/src/main/java/org/springframework/cloud/stream/app/function/app/FunctionProperties.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.app.function.app;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * Configuration properties for deciding how to locate the functional class to execute.
+ *
+ * @author Eric Bottard
+ */
+public class FunctionProperties {
+
+	/**
+	 * Location(s) of jar archives containing the supplier/function/consumer class to run.
+	 */
+	@Size(min = 1)
+	private String[] location;
+
+	/**
+	 * The fully qualified class name of the supplier/function/consumer to run.
+	 */
+	@NotBlank
+	private String className;
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String[] getLocation() {
+		return location;
+	}
+
+	public void setLocation(String[] location) {
+		this.location = location;
+	}
+}
---
+++ spring-cloud-starter-stream-app-function/src/test/java/org/springframework/cloud/stream/app/function/app/FunctionAppConfigurationTests.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.app.function.app;
+
+import java.util.concurrent.TimeUnit;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.cloud.stream.test.binder.MessageCollector;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = FunctionAppConfiguration.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@TestPropertySource(properties = {
+        "function.location=file:src/test/resources/function-sample-1.0.0.BUILD-SNAPSHOT.jar",
+        })
+public abstract class FunctionAppConfigurationTests {
+
+    @Autowired
+    protected MessageCollector messageCollector;
+
+
+    @EnableAutoConfiguration
+    @TestPropertySource(properties = {
+            "function.className=com.example.functions.Emitter"})
+    public static class SourceTests extends FunctionAppConfigurationTests {
+
+        @Autowired
+        private Source source;
+
+        @Test
+        public void test() throws Exception {
+
+            Message<?> received = messageCollector.forChannel(source.output()).poll(2, TimeUnit.SECONDS);
+            assertThat(received.getPayload(), Matchers.is("one"));
+
+        }
+
+    }
+
+    @EnableAutoConfiguration
+    @TestPropertySource(properties = {
+            "function.className=com.example.functions.LengthCounter"})
+    public static class ProcessorTests extends FunctionAppConfigurationTests {
+
+        @Autowired
+        private Processor processor;
+
+        @Test
+        public void test() throws Exception {
+            processor.input().send(MessageBuilder.withPayload("hello").build());
+            Message<?> received = messageCollector.forChannel(processor.output()).poll(1, TimeUnit.SECONDS);
+            assertThat(received.getPayload(), Matchers.is("hello".length()));
+
+        }
+
+    }
+
+    @EnableAutoConfiguration
+    @TestPropertySource(properties = {
+            "function.className=com.example.functions.DoubleLogger"})
+    public static class SinkTests extends FunctionAppConfigurationTests {
+
+        @Autowired
+        private Sink sink;
+
+        @Test
+        public void test() throws Exception {
+            // Can't assert side effects.
+            sink.input().send(MessageBuilder.withPayload(5).build());
+        }
+
+    }
+
+
+}
